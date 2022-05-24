e0848188c602092a86e9b570dc28c929bba35544
unknown <170100236@esg.ipsantarem.pt>
2022-05-24 18:09:02 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <CovidData></CovidData>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue"
+import CovidData from "@/components/CovidData.vue"
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    CovidData,
+  },
+}
+</script>
---
+++ src/components/CovidData.vue
@@ -0,0 +1,41 @@
+<template>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate"></DataTitle>
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+  </main>
+</template>
+<script>
+import DataTitle from "@/components/DataTitle.vue"
+
+export default {
+  name: "CovidData",
+  components: { DataTitle },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary")
+      const data = await res.json()
+      return data
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+    console.log(data)
+  },
+}
+</script>
